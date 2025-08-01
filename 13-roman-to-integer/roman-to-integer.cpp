001c72d8de2830e5d6956b18d5544ecc37aad438
MYGITACCOUNT-10 <147538687+MYGITACCOUNT-10@users.noreply.github.com>
2025-08-01 22:11:48 +0530

    Time: 5 ms (59.78%) | Memory: 13.4 MB (36.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,30 +1,26 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     int romanToInt(string s) {
-        int value=0;
-         unordered_map<char, int> roman = {
+        unordered_map<char, int> roman = {
             {'I',1}, {'V',5}, {'X',10}, {'L',50},
             {'C',100}, {'D',500}, {'M',1000}
         };
-
-        int i=0;
-       while(i<s.size())
-        {   
-            int curr=roman[s[i]];
-            int next=roman[s[i+1]];
-            if(curr<next)
-            {
-                value+=(next-curr);
-                i=i+2;
-            }
-            else{
-                value+=curr;
-                i++;
-            }
-
-        }
-
-        return value;
         
+        int result = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            int curr = roman[s[i]];
+            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
+            
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+        return result;
     }
 };
