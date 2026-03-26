a002fc1653f7738d9e08c3dcda8500979e363092
MYGITACCOUNT-10 <147538687+MYGITACCOUNT-10@users.noreply.github.com>
2026-03-27 00:22:58 +0530

    Time: 142 ms (5.05%) | Memory: 13.9 MB (93.94%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,9 +6,9 @@ public:
         
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) { 
-                if (nums[i] + nums[j] == target) {                result.push_back(i);  
+                if (nums[i] + nums[j] == target) {                
+                    result.push_back(i);  
                     result.push_back(j);  
-                    return result;
                 }
             }
         }
