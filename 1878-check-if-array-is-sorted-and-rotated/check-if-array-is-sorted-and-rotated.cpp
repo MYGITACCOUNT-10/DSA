2e9391353d5b1349072430fd5cd526d735a7e5aa
MYGITACCOUNT-10 <147538687+MYGITACCOUNT-10@users.noreply.github.com>
2026-05-23 10:34:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (18.08%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int dip=0;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>nums[(i+1)%n]) dip++;
+        }
+        return dip<=1;
+        
+    }
+};
